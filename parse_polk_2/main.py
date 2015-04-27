b9483e4b85c814caa4d5d5725c5882ebffd1c182
Steven Ourada <s@ourada.org>
2015-04-27 08:26:23 -0500

    cleanup

--- parse_polk_2/main.py
+++ parse_polk_2/main.py
@@ -2,45 +2,6 @@ import re
 import sys
 import argparse
 
-class Format1and2(object):
-    @staticmethod
-    def parse(lines):
-        # looks like an address
-        address = re.compile(r'[RrHh]?([1-9lI][0-9oOl]* +.{1,12} +(AV|Av|av|ST|St|st|RD|Rd|rd|DR|Dr|dr))\b')
-        # stuff we want to strip out
-        junk = re.compile(r'[*<>.]')
-        spaces = re.compile(r'\s+')
-
-        hits = []
-
-        # try 1-3 lines together until we get a match for the pattern
-        lineno = 0
-        while lineno < len(lines):
-            concat_lines = ''
-            for i in range(2):
-                if lineno + i >= len(lines):
-                    break
-                concat_lines += ' ' + lines[lineno + i]
-                m = address.search(concat_lines)
-                if m:
-                    hits.append(m.group(1))
-                    lineno += i
-                    break
-            lineno += 1
-
-        # fix up common errors
-        fixed_hits = []
-        for hit in hits:
-            fixed_hit = junk.sub('', hit)
-            fixed_hit = spaces.sub(' ', fixed_hit)
-            end_of_initial_number = fixed_hit.find(' ')
-            initial_number = fixed_hit[:end_of_initial_number]
-            initial_number = initial_number.replace('I', '1').replace('l', '1').replace('o', '0').replace('O', '0')
-            fixed_hit = initial_number + fixed_hit[end_of_initial_number:]
-            fixed_hits.append(fixed_hit)
-
-        return fixed_hits
-
 
 class Format3(object):
     @staticmethod
@@ -97,12 +58,12 @@ class Format3(object):
 
             hits.append(','.join(cols))
 
-        return hits
+        return [hits, err]
 
-format_map = {1: Format1and2, 2: Format1and2, 3: Format3}
+format_map = {3: Format3}
 if __name__ == '__main__':
     lines = []
-    arg_parser = argparse.ArgumentParser(description='Parse OCR data from phonebooks')
+    arg_parser = argparse.ArgumentParser(description='Parse OCR data from phonebooks. Outputs output.csv and errors.txt')
     arg_parser.add_argument('format', type=int, help='number of format to parse')
     arg_parser.add_argument('file', type=str, help='file to parse', nargs='+')
 
@@ -113,5 +74,8 @@ if __name__ == '__main__':
             lines += [l.strip() for l in f.readlines()]
 
     parser = format_map[parsed_args.format]()
-    for hit in parser.parse(lines):
-        print hit
+    output, errors = parser.parse(lines)
+    with open('output.csv', 'w') as out:
+        out.writelines([line + '\n' for line in output])
+    with open('errors.txt', 'w') as out:
+        out.writelines([line + '\n' for line in errors])
---
+++ parse_polk_2/readme.md
@@ -0,0 +1,15 @@
+usage: main.py [-h] format file [file ...]
+
+Parse OCR data from phonebooks. Outputs output.csv and errors.txt
+
+positional arguments:
+  format      number of format to parse
+  file        file to parse
+
+optional arguments:
+  -h, --help  show this help message and exit
+
+
+example:
+
+		python main.py 3 ../samples/Format_3/Text_Files/1935_Evens_Column_1.txt
